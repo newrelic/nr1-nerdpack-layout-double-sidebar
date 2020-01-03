7a6ffd17db6a578ca8bb7bf34a71299f8beb358f
Daniel Golden <dgolden@newrelic.com>
2020-01-03 08:22:18 -0500

    fix: broken repo link in empty state

--- nerdlets/nerdpack-layout-standard-nerdlet/index.js
+++ nerdlets/nerdpack-layout-standard-nerdlet/index.js
@@ -135,7 +135,7 @@ export default class NerdpackLayoutDoubleSidebar extends React.Component {
                     Have suggestions, concerns, or ideas for how this template
                     could be better? Feel free to{' '}
                     <a
-                      href="https://github.com/newrelic/nr1-nerdpack-layout-standard"
+                      href="https://github.com/newrelic/nr1-nerdpack-layout-double-sidebar"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
